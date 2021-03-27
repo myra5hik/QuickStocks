68d90e16077a5b67662ff5a7832cbb10ae9dd1a6
Alexander Tokarev <alexander.v.tokarev@gmail.com>
2021-03-27 17:55:33 +0300

    Change default tab of MainScreenVeiw to FinIndexView.

--- QuickStocks/UI/Screens/MainScreenTabView.swift
+++ QuickStocks/UI/Screens/MainScreenTabView.swift
@@ -9,23 +9,27 @@ import SwiftUI
 
 struct MainScreenTabView: View {
     @ObservedObject var viewModel: ViewModel
+    @State private var selection = 1
     
     var body: some View {
-        TabView(selection: .constant(0)) {
+        TabView(selection: $selection) {
             SearchView(viewModel: .init(container: viewModel.container))
                 .tabItem {
                     Label("Search", systemImage: "magnifyingglass")
                 }
+                .tag(0)
             
             FinIndexView(viewModel: .init(container: viewModel.container, indexSymbol: "^NDX"))
                 .tabItem {
                     Label("Nasdaq 100", systemImage: "list.dash")
                 }
+                .tag(1)
             
             FavouritesView(viewModel: .init(container: viewModel.container))
                 .tabItem {
                     Label("Favourites", systemImage: "list.star")
                 }
+                .tag(2)
         }
         .accentColor(Color("Pale Black"))
     }
